cef56e278391099c7c23835901469e3ce3127662
pietro <pietro1990@hotmail.it>
2020-07-13 21:11:01 +0200

    mancano le ultime crud

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1>hai fatto il login nella dashbord</h1>
+            </div>
+        </div>
+    </div>
+@endsection
