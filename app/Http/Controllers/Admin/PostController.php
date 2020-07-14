aa47e5b31fd63a665f4ee28bd77ca55113b51024
pietro <pietro1990@hotmail.it>
2020-07-14 07:38:29 +0200

    completato

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -37,7 +38,17 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255|unique:posts,title',
+            'content' => 'required'
+        ]);
+        $dati = $request->all();
+        $slug = Str::of($dati['title'])->slug('-');
+        $dati['slug'] = $slug;
+        $nuovo_post = new Post();
+        $nuovo_post->fill($dati);
+        $nuovo_post->save();
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -48,7 +59,12 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        if($post) {
+            return view('admin.posts.show', compact('post'));
+        } else {
+            return abort('404');
+        }
     }
 
     /**
@@ -59,7 +75,12 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+            if($post) {
+            return view('admin.posts.edit', compact('post'));
+        } else {
+            return abort('404');
+        }
     }
 
     /**
@@ -71,7 +92,16 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255|unique:posts,title,'.$id,
+            'content' => 'required'
+        ]);
+        $dati = $request->all();
+        $slug = Str::of($dati['title'])->slug('-');
+        $dati['slug'] = $slug;
+        $post = Post::find($id);
+        $post->update($dati);
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -82,6 +112,12 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        if($post) {
+            $post->delete();
+            return redirect()->route('admin.posts.index');
+        } else {
+            return abort('404');
+        }
     }
 }
